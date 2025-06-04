af79a9e3db85302d8fe46e9aeac9173dd6101c7e
Rayfer12 <147681271+Rayfer12@users.noreply.github.com>
2025-06-05 04:11:59 +0700

    db.js

--- config/db.js
+++ config/db.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB connected');
   } catch (err) {
-    console.error('MongoDB connection error:', err.message);
-    console.error(err.stack); // Add stack trace for better debugging
+    console.error(err.message);
     process.exit(1);
   }
 };
