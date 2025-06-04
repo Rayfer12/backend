9ad46a55f546eb68b30aff09592ff22e9da03db8
Rayfer12 <147681271+Rayfer12@users.noreply.github.com>
2025-06-05 04:32:14 +0700

    User.js

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false }
 });
 
 // Hash password before saving
@@ -19,7 +20,7 @@ userSchema.pre('save', async function (next) {
   }
 });
 
-// Compare password method for login
+// Password comparison method
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
